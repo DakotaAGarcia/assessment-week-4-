953364371684c6864ba2411eae2730f257219ceb
Dakota Garcia <dakotagarcia@Dakotas-MacBook-Air.local>
2023-01-16 18:13:35 -0700

    third commit

--- client/main.js
+++ client/main.js
@@ -1,19 +1,22 @@
 
-const form = document.querySelector('form').addEventListener("submit", e => {
-    e.preventDefault();
 const complimentBtn = document.getElementById("complimentButton");
 const fortuneBtn = document.getElementById("fortune");
 const addCompBtn = document.getElementById("add");
 const addQuoteBtn = document.getElementById("inspire");
 const deleteBtn = document.getElementById("delete");
+const createBtn = document.getElementById("create");
+
+const form = document.querySelector('form').addEventListener("submit", e => {
+    e.preventDefault();
+
 const getCompliment = () => {
     complimentBtn.disabled = true;
     axios.get("http://localhost:4000/api/compliment/")
     .then(res => {
         const data = res.data;
         alert(data);
-        complimentBtn.disabled = false;
     });
+    complimentBtn.disabled = false;
 };
 
 const getFortune = () =>{
@@ -22,17 +25,22 @@ const getFortune = () =>{
     .then(res =>{
         const data = res.data;
         alert(data);
-        fortuneBtn.disabled = false;
     })
     .catch((error) =>{
         console.log(error)
     })
+    fortuneBtn.disabled = false;
 };
 
 
 const addCompliment = () =>{
     addCompBtn.disabled = true;
     const newComp = document.getElementById("newComp").value;
+    if(newComp === ""){
+        alert("First you must type something before it can be added!")
+        addCompBtn.disabled = false;
+        return;
+    }
     axios.post("http://localhost:4000/api/add",
     {data: newComp})
     .then(res => {
@@ -40,17 +48,22 @@ const addCompliment = () =>{
         console.log(res.data)
         alert(data)
         document.getElementById('newComp').value ='';
-        addCompBtn.disabled = false;
     })
     .catch((error) =>{
         console.log(error);
     })
+    addCompBtn.disabled = false;
     
 };
 
 const addInspiration= () =>{
     addQuoteBtn.disabled = true;
     const newQuote = document.getElementById("quote").value;
+    if(newQuote === ""){
+        alert("Oops, can't add a quote that doesn't exist!")
+        addCompBtn.disabled = false;
+        return;
+    }
     axios.post("http://localhost:4000/api/quote",
     {data: newQuote})
     .then(res => {
@@ -58,16 +71,16 @@ const addInspiration= () =>{
         console.log(res.data)
         alert(data)
         const newQuote = document.getElementById("quote").value;
-    const newListItem = document.createElement("li");
-    newListItem.textContent = newQuote;
-    const quotesList = document.getElementById("quotes-list");
-    quotesList.appendChild(newListItem);
-
-        addQuoteBtn.disabled = false;
+        const newListItem = document.createElement("li");
+        newListItem.textContent = newQuote;
+        const quotesList = document.getElementById("quotes-list");
+        quotesList.appendChild(newListItem);
+        document.getElementById('quote').value ='';
     })
     .catch((error) =>{
         console.log(error);
     })
+    addQuoteBtn.disabled = false;
 };
 // const deleteQuote = () =>
 // deleteBtn.disabled = true;
@@ -91,14 +104,22 @@ const deleteQuote = () => {
         const quotesList = document.getElementById("quotes-list");
         const lastQuote = quotesList.lastChild;
         quotesList.removeChild(lastQuote);
-        deleteBtn.disabled = false;
-      })
-      .catch((error) => {
+    })
+    .catch((error) => {
         console.log(error);
-      });
+    });
+    deleteBtn.disabled = false;
   };
   
-
+  const createProfile = body =>{
+    createBtn.disabled = true;
+    axios.post("http://localhost:4000/api/create", body)
+    .then(res =>{
+        const data = res.data;
+        alert(data);
+        
+    })
+  }
 
 deleteBtn.addEventListener('click', deleteQuote)
 addQuoteBtn.addEventListener('click', addInspiration)
